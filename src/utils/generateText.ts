9a756784d89741e9b78e030e247b4b7dcae96fec
NToan324 <nhattoan664t@gmail.com>
2025-07-05 10:39:56 +0700

    update

--- src/utils/generateText.ts
+++ src/utils/generateText.ts
@@ -1,18 +1,18 @@
-const GoogleGenAI = require('@google/genai');
+const GoogleGenAI = require("@google/genai");
 
 export default async function generateContext(promt: string) {
-	const genAi = new GoogleGenAI({
-		apiKey: process.env.GOOGLE_API_KEY,
-	});
+  const genAi = new GoogleGenAI({
+    apiKey: process.env.GOOGLE_API_KEY,
+  });
 
-	const response = await genAi.models.generateContent({
-		model: 'gemini-2.5-pro',
-		contents: [
-			{
-				text: promt,
-			},
-		],
-	});
+  const response = await genAi.models.generateContent({
+    model: "gemini-2.5-pro",
+    contents: [
+      {
+        text: promt,
+      },
+    ],
+  });
 
-	return response.text;
+  return response.text;
 }
